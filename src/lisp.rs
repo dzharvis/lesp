bb244148db27451cd108d1e3623f202fc82f2cfa
dzharvis <dzharvis@gmail.com>
2019-02-25 14:01:08 +0200

    Add some tests

--- src/lisp.rs
+++ src/lisp.rs
@@ -4,12 +4,32 @@ use std::fmt::Error;
 use std::fmt;
 use std::rc::Rc;
 
+use lexer;
+use builder;
+use built_in;
+
 pub type Function = Fn(&mut Context, &[Type]) -> Type;
 pub type Context = HashMap<String, Type>;
 
 #[derive(Clone)]
 pub enum Type {
     Symbol(String), Bool(bool), Number(u32), List(Vec<Type>), Function(String, Rc<Function>)
+}
+
+impl PartialEq for Type {
+    fn eq(&self, other: &Type) -> bool {
+        match (self, other) {
+            (Type::Function(name, rc), Type::Function(name_other, rc_other))  => {
+                // should work, probably xD
+                name.eq(name_other) && (rc.as_ref() as *const _ ==  rc_other.as_ref() as *const _)
+            },
+            (Type::List(elems), Type::List(elems_other)) => elems.eq(elems_other),
+            (Type::Number(n), Type::Number(n_other)) => n.eq(n_other),
+            (Type::Symbol(s), Type::Symbol(s_other)) => s.eq(s_other),
+            (Type::Bool(b), Type::Bool(b_other)) => b.eq(b_other),
+            (_,_) => false
+        }
+    }
 }
 
 impl fmt::Debug for Type {
@@ -47,5 +67,87 @@ impl Eval for Type {
             },
             Type::Function(_name, _f) => unimplemented!()
         }
+    }
+}
+
+pub fn eval(input: &String) -> Type {
+    let res = lexer::parse_fsm(&input);
+    let (n, _) = builder::build(&res, 0);
+    let mut context = built_in::init_context();
+    n.eval(&mut context)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_forms() {
+        assert_eq!(eval(&String::from("(+ 1 2)")), Type::Number(3));
+        assert_eq!(eval(&String::from("(* 2 2)")), Type::Number(4));
+        assert_eq!(eval(&String::from("(> 4 2)")), Type::Bool(true));
+        assert_eq!(eval(&String::from("(- 4 2)")), Type::Number(2));
+    }
+
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(eval(&String::from("(let ((fib (fn fib (n) \
+                                                (if (> 3 n)\
+                                                    (- n 1)\
+                                                    (+ (fib (- n 1))\
+                                                    (fib (- n 2)))))))\
+                                        (fib 8))")),
+                   Type::Number(13));
+    }
+
+    #[test]
+    fn test_higher_order_functions() {
+        assert_eq!(eval(&String::from("(let ((apply (fn apply (f n) (f (n)))))\
+                                        (apply (fn _ (a) (* a a)) (fn _ () 10)))")),
+                   Type::Number(100));
+    }
+
+    #[test]
+    fn test_closures() {
+        assert_eq!(eval(&String::from("(let ((a 100)\
+                                            (adda (fn adda (n) (+ a n))))\
+                                        (adda 3))")),
+                   Type::Number(103));
+    }
+
+    #[test]
+    fn test_closures_with_recursion() {
+        assert_eq!(eval(&String::from("(let ((one 1)\
+                                            (three 3)\
+                                            (two 2)\
+                                            (fib (fn fib (n) \
+                                                (if (> three n)\
+                                                    (- n one)\
+                                                    (+ (fib (- n one))\
+                                                    (fib (- n two)))))))\
+                                        (fib 8))")),
+                   Type::Number(13));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_nested_scope_invisible() {
+        assert_eq!(eval(&String::from("(let ((a (let ((b 1)(c 2)) (+ b c))))\
+                                            b)")),
+                   Type::Number(13));
+    }
+
+    #[test]
+    fn test_nested_scope_invisible_proper_setup() {
+        assert_eq!(eval(&String::from("(let ((a (let ((b 1)(c 2)) (+ b c))))\
+                                            a)")),
+                   Type::Number(3));
+    }
+
+    #[test]
+    fn test_closure_captures_nested_context() {
+        assert_eq!(eval(&String::from("(let ((a (let ((b 1)(c 2)) (fn _ () (+ b c)))))\
+                                            (a))")),
+                   Type::Number(3));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate lazy_static;
 extern crate regex;
 extern crate core;
 
@@ -10,28 +9,5 @@ mod built_in;
 use lisp::Eval;
 
 fn main() {
-//    let input = String::from("(let ((add (fn add () (fn a (a b) (+ a b))))\
-//                                               (apply (fn apply (f a) (f a)))
-//                                                (fib (fn fib (n) \
-//                                                (if (> 3 n)\
-//                                                    (- n 1)\
-//                                                    (+ (fib (- n 1)) \
-//                                                       (fib (- n 2)))))))\
-//                                          (fib 6)\
-//                                          ((add) 2 4)\
-//                                          (apply (fn _ (a) (* a 10) ) 123))");
-
-    let input = String::from("(let ((apply (fn apply (f a) (f a))))\
-                                          (apply (fn _ (a) (* a a) ) ((fn _ (a) (+ a 5)) 5)))");
-//    println!("input -> {}", input);
-
-    let res = lexer::parse_fsm(&input);
-//    println!("lexer -> {:?}", res);
-
-    let (n, _) = builder::build(&res, 0);
-    println!("tree -> {:?}", n);
-
-    let mut context = built_in::init_context();
-    let result = n.eval(&mut context);
-    println!("result -> {:?}", result);
+    println!("result -> {:?}", lisp::eval(&String::from("(+ 1 2)")));
 }

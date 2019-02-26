65fb4bc14a6166d706706d8a148706e407d4c4b0
dzharvis <dzharvis@gmail.com>
2019-02-26 11:49:37 +0200

    Implement very primitive PERL
    No support for multiline forms for now

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,30 @@ mod lisp;
 mod parser;
 mod built_in;
 
+use std::io::{self, BufRead, Write};
+
 fn main() {
     let mut context = built_in::init_context();
-    lisp::eval_in_context(&String::from("(def a 1)"), &mut context);
-    let r = lisp::eval_in_context(&String::from("(+ a 3)"), &mut context);
-    println!("{:?}", r);
+
+    init_context_from_file(&mut context);
+
+    let stdin = io::stdin();
+    print(">> ");
+    
+    for line in stdin.lock().lines() {
+        let r = lisp::eval_in_context(&String::from(line.unwrap()), &mut context);
+        println!("<< {:?}", r);
+        print(">> ");
+    }
+}
+
+fn print(s: &str) {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write(&s.as_bytes());
+    handle.flush();
+}
+
+fn init_context_from_file(mut context: &mut lisp::Context) {
+
 }
